4d565b7cb31e575129d45f7535919377c4e15f07
root <43900374>
2023-02-16 17:35:51 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: always 0 to indicate succesful program completion
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
