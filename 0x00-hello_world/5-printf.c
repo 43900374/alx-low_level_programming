9044f0cb3d7cb3ef484b26566ebdd32e8c7390db
root <43900374>
2023-02-16 18:23:28 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the project
+ *
+ * Return: always 0 to indicate successful program completion
+ */
+int main(void)
+{
+	printf("with proper grammar,but the outcomeis a piece of art,\n");
+	return (0);
+}
