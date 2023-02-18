262991d66b970a7b3e3b29595873f61de2a6454c
root <43900374>
2023-02-17 18:06:42 -0800

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point for the project
- *
- * Return: always 0 to indicate successful program completion
- */
+* main - entry point for the program
+* Description: Program that prints out the string s followed by a new line
+* Return: always 0 to indicate successful program completion
+*/
 
 int main(void)
 {
-	printf("with proper grammar, but the outcomeis a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
