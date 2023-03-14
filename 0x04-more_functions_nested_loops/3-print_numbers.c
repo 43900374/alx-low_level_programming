ff14b7e5057352a59fa2ffd050e23f480a8cae4d
43900374 <cynthia.k.wairimu@gmail.com>
2023-03-14 12:00:32 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,21 @@
+#include <unistd.h>
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_numbers - Entry point
+ * Description: print 0-9
+ * Return: void
  */
-int main(void)
+
+void print_numbers(void)
 {
-    print_numbers();
-    return (0);
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+
+	_putchar('\n');
 }
